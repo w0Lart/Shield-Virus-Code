8b98d2bfda5178dc09d4c77c2da29b5dbbd73037
w0Lart <olegkostuk2009@gmail.com>
2023-09-15 12:06:24 +0300

    make click buttons starting the game

--- js/scripts.js
+++ js/scripts.js
@@ -5,28 +5,32 @@ function getComputerChoice () {
   return (resultComputerChoice);
 }
 
-function getUserChoice () {
-  let takeUserInput = prompt("Make your choice, type: 'Shield' or 'Virus' or 'Code'");
-  (takeUserInput == null) ? alert("error, you must type something") : takeUserInput = takeUserInput.toLowerCase();
-  if (takeUserInput === "shield") {
-    return "Shield";
-  } else if (takeUserInput === "virus") {
-    return "Virus";
-  } else if (takeUserInput === "code") {
-    return "Code";
-  } else {
-    return (alert("Wrong choice!"));
-  }
-}
+let buttons= document.querySelector('.chooseButtons');
 
+
+
+//function getUserChoice () {
+//  let takeUserInput = addEventListener('click', buttonChoice);
+//  (takeUserInput == null) ? alert("error, you must type something") : takeUserInput = takeUserInput.toLowerCase();
+//  if (takeUserInput === "shield") {
+//    return "Shield";
+//  } else if (takeUserInput === "virus") {
+//    return "Virus";
+//  } else if (takeUserInput === "code") {
+//    return "Code";
+//  } else {
+//    return (alert("Wrong choice!"));
+//  }
+//}
 let roundCounter = 0;
 let counterWin = 0;
 let counterLoose = 0;
 let counterDraw = 0;
 
-function playRound () {
-  
-  let humanPlayer = getUserChoice();
+
+function playRound (userchoice) {
+
+  let humanPlayer = userchoice;
   let computerPlayer = getComputerChoice();
   console.log("player choose", humanPlayer);
   console.log("PC choose", computerPlayer);
@@ -37,56 +41,81 @@ function playRound () {
     roundWinner = "You win! Shield beat Virus";
     roundCounter++;
     counterWin++;
+    console.log(roundWinner);
+    return (roundWinner);
   } else if (humanPlayer == "Shield" && computerPlayer == "Code") {
     roundLooser = "You loose! Shield loose to Code";
     roundCounter++;
     counterLoose++;
+    console.log(roundLooser);
+    return (roundLooser);
   } else if (humanPlayer == "Virus" && computerPlayer == "Code") {
     roundWinner = "You win! Virus beat Code";
     roundCounter++;
     counterWin++;
+    console.log(roundWinner);
+    return (roundWinner);
   } else if (humanPlayer == "Virus" && computerPlayer == "Shield") {
     roundLooser = "You loose! Virus loose to Shield";
     roundCounter++;
     counterLoose++;
+    console.log(roundLooser);
+    return (roundLooser);
   } else if (humanPlayer == "Code" && computerPlayer == "Shield") {
     roundWinner = "You win! Code beat Shield";
     roundCounter++;
     counterWin++;
+    console.log(roundWinner);
+    return (roundWinner);
   } else if (humanPlayer == "Code" && computerPlayer == "Virus") {
     roundLooser = "You loose! Code loose to Virus";
     roundCounter++;
     counterLoose++;
+    console.log(roundLooser);
+    return (roundLooser);
   } else if (humanPlayer == "Shield" && computerPlayer == "Shield") {
     roundDraw = "Draw for shields!";
     roundCounter++;
     counterDraw++;
+    console.log(roundDraw);
+    return (roundDraw);
   } else if (humanPlayer == "Virus" && computerPlayer == "Virus") {
     roundDraw = "Draw for virus!";
     roundCounter++;
     counterDraw++;
+    console.log(roundDraw);
+    return (roundDraw);
   } else if (humanPlayer == "Code" && computerPlayer == "Code") {
     roundDraw = "Draw for Code!";
     roundCounter++;
     counterDraw++;
+    console.log(roundDraw);
+    return (roundDraw);
   } else {
     return "Something went wrong! Let's try again";
   }
+  
 
-  if (roundWinner == "You win! Shield beat Virus" || roundWinner == "You win! Virus beat Code" || roundWinner == "You win! Code beat Shield"){
-    return (roundWinner);
-  } else if (roundLooser == "You loose! Shield loose to Code" || roundLooser == "You loose! Virus loose to Shield" || roundLooser == "You loose! Code loose to Virus"){
-    return (roundLooser);
-  } else if (roundDraw == "Draw for shields!" || roundDraw == "Draw for virus!" || roundDraw == "Draw for Code!") {
-    return (roundDraw);
-  } else {
-    return ("can't find a winner, let's try again");
-  }
+  //if (roundWinner == "You win! Shield beat Virus" || roundWinner == "You win! Virus beat Code" || roundWinner == "You win! Code beat Shield"){
+  //  return (roundWinner);
+  //} else if (roundLooser == "You loose! Shield loose to Code" || roundLooser == "You loose! Virus loose to Shield" || roundLooser == "You loose! Code loose to Virus"){
+  //  return (roundLooser);
+  //} else if (roundDraw == "Draw for shields!" || roundDraw == "Draw for virus!" || roundDraw == "Draw for Code!") {
+  //  return (roundDraw);
+  //} else {
+  //  return ("can't find a winner, let's try again");
+  //}
   
 
 }
 
+buttons.addEventListener('click', function(event) {
+  if (event.target.tagName === "BUTTON") {
+    let buttonText = event.target.innerText;
 
+    playRound(buttonText);
+    checkWinner();
+}});
 
 function checkWinner() {
   if (roundCounter === 5) {
